653ad983414a2cac0049a77865f75d8208f1f381
Mateus Miguel <webmateusmiguel@gmail.com>
2017-09-26 16:13:03 -0300

    Listando dados

--- course/js/main.js
+++ course/js/main.js
@@ -1,15 +1,15 @@
 var list = [
-    {"desc":"rice","amount":"1", "value":"5.40"},
-    {"desc":"beer","amount":"12", "value":"1.99"},
-    {"desc":"meat","amount":"1", "value":"15.00"}
+    { "desc": "rice", "amount": "1", "value": "5.40" },
+    { "desc": "beer", "amount": "12", "value": "1.99" },
+    { "desc": "meat", "amount": "1", "value": "15.00" }
 ];
 
-function getTotal (list) {
+function getTotal(list) {
     var total = 0;
     for (var key in list) {
         total += list[key].value * list[key].amount;
     }
-    
+
     // Equivalente:
 
     // for (i = 0; i < list.length; i++) { 
@@ -20,4 +20,13 @@ function getTotal (list) {
     return total;
 }
 
-console.log (getTotal(list));
+function setList(list) {
+    var table = '<thead><tr><th>Description</th><th>Amount</th><th>Value</th><th>Action</th></tr></thead><tbody>';
+    for (var key in list) {
+        table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td><a href="#">Edit</a> | <a href="#">Delete</a></td></tr>'
+    }
+    table += '</tbody>';
+    document.getElementById("listTable").innerHTML = table;
+}
+setList(list);
+console.log(getTotal(list));
