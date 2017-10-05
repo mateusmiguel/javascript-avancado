0de59e0c1cffc6b48a006db01066168605b75b41
Mateus Miguel <webmateusmiguel@gmail.com>
2017-10-05 10:23:23 -0300

    Formatando strings e valores

--- course/js/main.js
+++ course/js/main.js
@@ -23,10 +23,25 @@ function getTotal(list) {
 function setList(list) {
     var table = '<thead><tr><th>Description</th><th>Amount</th><th>Value</th><th>Action</th></tr></thead><tbody>';
     for (var key in list) {
-        table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td><a href="#">Edit</a> | <a href="#">Delete</a></td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><a href="#">Edit</a> | <a href="#">Delete</a></td></tr>'
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
 }
+
+function formatDesc(desc) {
+    var str = desc.toLowerCase();
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    return str;
+}
+
+function formatValue(value) {
+    var str = parseFloat(value).toFixed(2) + ""; //concatenar com valor vazio para voltar para string
+    str = str.replace(".", ",");
+    str = "R$ " + str;
+    return str;
+
+}
+
 setList(list);
 console.log(getTotal(list));
