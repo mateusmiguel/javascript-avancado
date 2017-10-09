143186c6488f0b060a5767a8c6246c246d7e5df9
Mateus Miguel <webmateusmiguel@gmail.com>
2017-10-09 10:53:30 -0300

    Validação

--- course/js/main.js
+++ course/js/main.js
@@ -23,7 +23,7 @@ function getTotal(list) {
 function setList(list) {
     var table = '<thead><tr><th>Description</th><th>Amount</th><th>Value</th><th>Action</th></tr></thead><tbody>';
     for (var key in list) {
-    table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+');" class="btn btn-default">Edit</button> <button class="btn btn-default">Delete</button></td></tr>'
+    table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ formatAmount(list[key].amount) +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+');" class="btn btn-default">Edit</button> <button onclick="deleteData('+key+');" class="btn btn-default">Delete</button></td></tr>'
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -35,6 +35,11 @@ function formatDesc(desc) {
     return str;
 }
 
+function formatAmount(amount) {
+    return parseInt(amount);
+
+}
+
 function formatValue(value) {
     var str = parseFloat(value).toFixed(2) + ""; //concatenar com valor vazio para voltar para string
     str = str.replace(".", ",");
@@ -44,6 +49,9 @@ function formatValue(value) {
 }
 
 function addData () {
+    if(!validation()){
+        return;
+    }
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
@@ -61,9 +69,10 @@ function setUpdate(id) {
     var obj = list[id];
     document.getElementById("desc").value = obj.desc;
     document.getElementById("amount").value = obj.amount;
-    document.getElementById("value").value = obj.desc;
+    document.getElementById("value").value = obj.value;
     document.getElementById("btnUpdate").style.display = "inline-block";  
-    document.getElementById("btnAdd").style.display = "none";  
+    document.getElementById("btnAdd").style.display = "none"; 
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+ id +'">';
 }
 
 function resetForm(id) {
@@ -72,6 +81,76 @@ function resetForm(id) {
     document.getElementById("value").value = "";
     document.getElementById("btnUpdate").style.display = "none";  
     document.getElementById("btnAdd").style.display = "inline-block";  
+    document.getElementById("inputIDUpdate").innerHTML = "";
+    document.getElementById("errors").style.display = "none";    
+}
+
+function updateData() {
+    if(!validation()){
+        return;
+    }
+    var id = document.getElementById("idUpdate").value;
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    
+    list[id] = {"desc": desc, "amount": amount, "value": value };
+    resetForm();
+    setList(list);
+
+}
+
+function deleteData(id) {
+    if(confirm ("Delete this item?")) {
+        if(id === list.length - 1) {//seleciona o último regirsto
+            list.pop(); //limpa o último registro da lista    
+        } else if(id === 0) { // seleciona a primeiro
+            list.shift(); //limpa o primeiro item de uma array
+        } else {
+            var arrAuxIni = list.slice(0, id); //pega do ínicio até o array
+            var arrAuxEnd = list.slice(id + 1); // pega o valor depois id até o final do array         
+            list = arrAuxIni.concat(arrAuxEnd); //concatena e junta as arrays
+        }
+        setList(list); //remonta a lista  
+    }
+}
+
+function validation() {
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    var errors = "";
+    document.getElementById("errors").style.display = "none";      
+    
+    if(desc === "") {
+        errors += '<p>Fill out description</p>';
+
+    }
+    if (amount ==="") {
+        errors += '<p>Fill out quantity</p>';
+        
+    } else if(amount != parseInt(amount)) {
+        errors += '<p>Fill out a valid amount</p>';        
+    }
+    if (value ==="") {
+        errors += '<p>Fill out value</p>';
+        
+    } else if(value != parseFloat(value)) {
+        errors += '<p>Fill out a valid value</p>';        
+    }
+
+    if (errors!= "") {
+        document.getElementById("errors").style.display = "block";      
+        document.getElementById("errors").style.backgroundColor = "#ddd";
+        document.getElementById("errors").style.color = "#fff";
+        document.getElementById("errors").style.padding = "10px"; 
+        document.getElementById("errors").style.margin = "10px 0"; 
+        document.getElementById("errors").style.borderRadius = "4px"; 
+        document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+        return 0; //
+    } else {
+        return 1;
+    }
 }
 
 setList(list);
